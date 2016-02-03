7dbad506e055f375bd7c2304df92b5e1846d0b9c
Therry van Neerven <therryvanneerven@gmail.com>
2016-02-03 21:18:30 +0100

    Activated windmove

--- init.el
+++ init.el
@@ -244,6 +244,7 @@ in `dotspacemacs/user-config'."
   "Configuration function for user code.
 This function is called at the very end of Spacemacs initialization after
 layers configuration. You are free to put any user code."
+  (windmove-default-keybindings)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
