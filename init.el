9122da2e5a0d7ed17b7f97fd2074f8f45ec427b0
Therry van Neerven <mail@therryvanneerven.nl>
2017-04-11 12:58:36 +0200

    sort imports on save

--- init.el
+++ init.el
@@ -286,3 +286,15 @@ layers configuration. You are free to put any user code."
          "\\)"
          ))
   '(python-shell-interpreter "python3"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((python-sort-imports-on-save t)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
